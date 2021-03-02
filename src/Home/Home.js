bcb74f259dc47ddc0719e2589604d9f17b0ea998
anieri <andre@nieri.com.br>
2021-03-02 00:30:42 -0300

    Add home button

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,12 +1,16 @@
 import { React } from 'react';
 import { hiragana, katakana } from '../data/languages';
 import CheckContainer from '../alphabet/check-container';
+import Button from '@material-ui/core/Button';
 import './Home.css';
 
 const Home = () => (
   <div className="home">
-    <CheckContainer alphabet={hiragana} alphabetType="hiragana" />
-    <CheckContainer alphabet={katakana} alphabetType="katakana" />
+    <div className="home-alphabets">
+      <CheckContainer alphabet={hiragana} alphabetType="hiragana" />
+      <CheckContainer alphabet={katakana} alphabetType="katakana" />
+    </div>
+    <Button className="play-button" variant="contained" color="primary">Game!</Button>
   </div>
 );
 
